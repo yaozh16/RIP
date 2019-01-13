4458dc493afbae954640e14814e37b89e4e80bcb
yaozh16 <yaozh16@qq.com>
2019-01-13 20:16:41 +0800

    如果编译不了可以直接用可执行文件

---
+++ cmake-build-debug/CMakeFiles/Test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Test.dir/rip.c.o"
+  "Test.pdb"
+  "Test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
